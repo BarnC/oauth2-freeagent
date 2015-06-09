e2cc0906178c4eaa029e08c52cd214eb6d0b516b
Barnaby Cotter <Barnaby@parall.ax>
2015-06-09 09:44:19 +0100

    add create contact method

--- src/Provider/Freeagent.php
+++ src/Provider/Freeagent.php
@@ -3,6 +3,8 @@
 namespace CloudManaged\OAuth2\Client\Provider;
 
 use CloudManaged\OAuth2\Client\Entity\Company;
+use Guzzle\Http\Exception\BadResponseException;
+use League\OAuth2\Client\Exception\IDPException;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
 
@@ -39,11 +41,42 @@ class FreeAgent extends AbstractProvider
         return $this->baseURL . 'company';
     }
 
+    public function urlCreateContact()
+    {
+        return $this->baseURL . 'contacts';
+    }
+
     public function userDetails($response, AccessToken $token)
     {
         $response = (array)($response->company);
         $company = new Company($response);
-
         return $company;
+    }
+
+    public function createContact(AccessToken $token, Array $data)
+    {
+        $url = $this->urlCreateContact($token);
+        $headers = $this->getHeaders($token);
+        $contact = (array)(json_decode($this->sendProviderData($url, $headers, $data))->contact);
+        // Free agent doesn't return the id as its own field, so we parse it out of the URL which IS supplied.. Thanks Freeagent.
+        $contact['id'] = explode('/contacts/', $contact['url'])[1];
+        return $contact;
+    }
+
+    protected function sendProviderData($url, array $headers = [], $data)
+    {
+        try {
+            $client = $this->getHttpClient();
+            $request = $client->post($this->urlCreateContact(), $headers, $data);
+            $response = $request->send()->getBody();
+        } catch (BadResponseException $e) {
+            // @codeCoverageIgnoreStart
+            $response = $e->getResponse()->getBody();
+            $result = $this->prepareResponse($response);
+            throw new IDPException($result);
+            // @codeCoverageIgnoreEnd
+        }
+
+        return $response;
     }
 }
